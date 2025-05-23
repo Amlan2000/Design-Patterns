774d6ec46b43319034e3728913a2a7977d54223d
Amlan Mishra <amlan@Amlans-MacBook-Air.local>
2025-05-23 22:51:51 +0530

    Singleton example added

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import Singleton.Printer;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, CloneNotSupportedException {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!\n");
+
+        Printer p1 = Printer.getPrinter();
+        System.out.println(p1.hashCode());
+
+        Printer p2 = Printer.getPrinter();
+        System.out.println(p2.hashCode());
+
+        //Reflection API , a way to access private constructor and break singleton pattern
+
+//        Constructor<Printer> constructor = Printer.class.getDeclaredConstructor();
+//        constructor.setAccessible(true);
+//        Printer p3 = constructor.newInstance();
+//        System.out.println(p3.hashCode());
+
+
+        // If Singleton implements Cloneable, you can call .clone() and get a new instance.
+
+        Printer p4 = Printer.getPrinter();
+        System.out.println(p4.hashCode());
+        Printer p5 = (Printer) p4.clone();
+        System.out.println(p5.hashCode());
+
+
+
+    }
+}
---
+++ src/Singleton/Printer.java
@@ -0,0 +1,32 @@
+package Singleton;
+
+public class Printer implements Cloneable{
+
+    private static Printer printerInstance;
+
+    private Printer(){
+        if(printerInstance!=null)    // this if block is added to prevent reflection API to create another object
+            throw new RuntimeException("Object has already been created");
+    }
+
+    public static Printer getPrinter(){  // We do not make this method synchronised as it will also block methods other than object creation. We only want to block the object creation
+
+        System.out.println("Inside getPrinterMethod() \n");
+
+        if(printerInstance==null) {         //if object is already created, no need to synchronise it and block the execution
+
+            synchronized (Printer.class) {  // we use synchronized to prevent multithreading object creation
+                if (printerInstance == null)
+                    printerInstance = new Printer();
+            }
+        }
+        return printerInstance;
+
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+//        return super.clone();   // this will create another object
+        return new CloneNotSupportedException("Cloning not supported for singleton");
+    }
+}
